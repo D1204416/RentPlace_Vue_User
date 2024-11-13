eb49ff8bc0aa32feb3152da32257157e7c8c0d66
D1204480 <161423607+D1204480@users.noreply.github.com>
2024-11-13 21:45:18 +0800

    Update Login.vue
    修正, 登入成功後關閉modal, 並跳轉到首頁

--- User_Frontend/src/components/Login.vue
+++ User_Frontend/src/components/Login.vue
@@ -9,24 +9,8 @@
       </div>
     </div>
 
-    <!-- google button -->
-    <!-- <button class="google-button">
-      <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 48 48" @click="handleGoogleLogin">
-        <path fill="#FFC107"
-          d="M43.611,20.083H42V20H24v8h11.303c-1.649,4.657-6.08,8-11.303,8c-6.627,0-12-5.373-12-12 s5.373-12,12-12c3.059,0,5.842,1.154,7.961,3.039l5.657-5.657C34.046,6.053,29.268,4,24,4C12.955,4,4,12.955,4,24s8.955,20,20,20  s20-8.955,20-20C44,22.659,43.862,21.35,43.611,20.083z" />
-        <path fill="#FF3D00"
-          d="M6.306,14.691l6.571,4.819C14.655,15.108,18.961,12,24,12c3.059,0,5.842,1.154,7.961,3.039 l5.657-5.657C34.046,6.053,29.268,4,24,4C16.318,4,9.656,8.337,6.306,14.691z" />
-        <path fill="#4CAF50"
-          d="M24,44c5.166,0,9.86-1.977,13.409-5.192l-6.19-5.238C29.211,35.091,26.715,36,24,36  c-5.202,0-9.619-3.317-11.283-7.946l-6.522,5.025C9.505,39.556,16.227,44,24,44z" />
-        <path fill="#1976D2"
-          d="M43.611,20.083H42V20H24v8h11.303c-0.792,2.237-2.231,4.166-4.087,5.571 c0.001-0.001,0.002-0.001,0.003-0.002l6.19,5.238C36.971,39.205,44,34,44,24C44,22.659,43.862,21.35,43.611,20.083z" />
-      </svg>
-    </button> -->
-
-
     <!-- Google 登入按鈕容器 -->
     <div id="googleButtonDiv" class="google-button-container"></div>
-
 
     <div class="social-buttons">
       <!-- meta button -->
@@ -107,7 +91,6 @@ const api = axios.create({
 
 // 處理Google登入
 const handleCredentialResponse = async (response) => {
-
   try {
     // 1.解碼 Google 回傳的 credential (JWT token)
     const credential = response.credential
@@ -149,9 +132,15 @@ const handleCredentialResponse = async (response) => {
       roles: user.roles // 如果有角色權限
     })
 
-    // 8. 導航到首頁或儀表板
-    router.push('/')
+    // 8. 關閉 Modal
+     const loginModal = document.getElementById('loginModal')
+      const bootstrapModal = bootstrap.Modal.getInstance(loginModal)
+      if (bootstrapModal) {
+        bootstrapModal.hide()
+      }
 
+    // 9. 導航到首頁或儀表板
+    router.push('/')
     console.log('Google 登入成功！')
 
   } catch (err) {
@@ -221,6 +210,13 @@ const handleLogin = async () => {
 
       // 設置 axios 默認 header
       api.defaults.headers.common['Authorization'] = `Bearer ${response.data.token}`
+
+      // 關閉 Modal
+      const loginModal = document.getElementById('loginModal')
+      const bootstrapModal = bootstrap.Modal.getInstance(loginModal)
+      if (bootstrapModal) {
+        bootstrapModal.hide()
+      }
 
       // 登入成功後跳轉首頁
       console.log("登入成功")
