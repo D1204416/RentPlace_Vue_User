2b35105f5032d6c09582a3282af552e9b7ab9215
D1204416 <161418864+D1204416@users.noreply.github.com>
2024-11-15 11:18:15 +0800

    調整位置

--- User_Frontend/src/App.vue
+++ User_Frontend/src/App.vue
@@ -8,27 +8,35 @@ import Foot from './components/Foot.vue';
 </script>
 
 <template>
-  <SideBar />
 
-  <!-- 登入模態框 -->
-  <div class="modal fade" id="loginModal" tabindex="-1" aria-labelledby="loginModalLabel" aria-hidden="true">
-    <div class="modal-dialog modal-dialog-centered">
-      <div class="modal-content">
-        <div class="modal-header">
-          <h5 class="modal-title" id="loginModalLabel"><img src="/img/logo.svg" alt="Logo" style="height: 35px;" /></h5>
-          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-        </div>
-        <div class="modal-body">
-          <Login @login-success="handleLoginSuccess"/>
+  <div id="app">
+    <div class="main-content">
+
+      <!-- 導覽列 -->
+      <SideBar />
+
+      <!-- 登入模組 -->
+      <div class="modal fade" id="loginModal" tabindex="-1" aria-labelledby="loginModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h5 class="modal-title" id="loginModalLabel"><img src="/img/logo.svg" alt="Logo" style="height: 35px;" />
+              </h5>
+              <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+              <Login @login-success="handleLoginSuccess" />
+            </div>
+          </div>
         </div>
       </div>
+
+      <!-- RoutrtView 切換分頁 -->
+      <RouterView />
+
     </div>
+    <Foot />
   </div>
-
-  <!-- RoutrtView 切換分頁 -->
-  <RouterView />
-
-  <Foot />
 
 </template>
 
@@ -40,5 +48,22 @@ const handleLoginSuccess = (userInfo) => {
 </script>
 
 <style scoped>
+
+.main-content {
+  flex: 1; /* 主內容填滿多餘空間 */
+}
+
+html, body {
+  height: 100%; /* 設定 html 和 body 滿高 */
+  margin: 0;
+  display: flex;
+  flex-direction: column;
+}
+
+#app {
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh; /* 確保內容至少佔滿視窗高度 */
+}
 
 </style>
