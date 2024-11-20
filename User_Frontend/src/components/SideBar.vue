0726af9d8bc3ab798079a8e68a6e023278fb2489
D1204416 <161418864+D1204416@users.noreply.github.com>
2024-11-20 09:39:12 +0800

    改變連結
    Sidebar台中通連到台中通官方首頁

--- User_Frontend/src/components/SideBar.vue
+++ User_Frontend/src/components/SideBar.vue
@@ -117,7 +117,7 @@
               <ul class="navbar-nav justify-content-end flex-grow-1 pe-3" style="margin-left: 30px;">
 
                 <li class="nav-item">
-                  <RouterLink to="/" class="nav-link" aria-current="page" style="color: #4A4A4A;">
+                  <a href="https://tcpass.taichung.gov.tw/" class="nav-link" aria-current="page" style="color: #4A4A4A;">
 
                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
                       class="bi bi-caret-right-fill" viewBox="0 0 16 16">
@@ -125,7 +125,7 @@
                         d="m12.14 8.753-5.482 4.796c-.646.566-1.658.106-1.658-.753V3.204a1 1 0 0 1 1.659-.753l5.48 4.796a1 1 0 0 1 0 1.506z" />
                     </svg>
                     台中通
-                  </RouterLink>
+                  </a>
                 </li>
 
                 <li class="nav-item">
