75a42e64ef978278ed175b311cf2caf0b2112c50
呂雅慈 <161418704+D1204504@users.noreply.github.com>
2024-12-25 09:58:04 +0800

    1

--- User_Frontend/vite.config.js
+++ User_Frontend/vite.config.js
@@ -1,5 +1,4 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
@@ -11,6 +10,15 @@ export default defineConfig({
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8080', // 後端服務的地址
+        changeOrigin: true, // 更改請求的來源，避免跨域問題
+         // 可選，如果後端路徑需要去掉 `/api`
+      }
     }
   }
 })
